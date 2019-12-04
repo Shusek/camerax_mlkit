7e796315795bb26302addef8a58f9c832019224f
Marcin suska <marcin.suska.ext@vodeno.net>
2019-12-04 20:45:41 +0100

    Add card scanner analyser

---
+++ app/src/main/java/pl/shu/camerax/sample/card/CardScannerAnalyser.kt
@@ -0,0 +1,26 @@
+package pl.shu.camerax.sample.card
+
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageProxy
+import com.google.firebase.ml.vision.FirebaseVision
+import com.google.firebase.ml.vision.common.FirebaseVisionImage
+import com.google.firebase.ml.vision.common.FirebaseVisionImageMetadata
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.tasks.await
+
+class CardScannerAnalyser : ImageAnalysis.Analyzer {
+
+    private val firebaseAnalyzer by lazy { FirebaseVision.getInstance().onDeviceTextRecognizer }
+
+    override fun analyze(image: ImageProxy, rotationDegrees: Int) {
+        val imageToAnalize = image.image ?: return
+        val mediaImage = FirebaseVisionImage.fromMediaImage(
+            imageToAnalize, FirebaseVisionImageMetadata.ROTATION_0
+        )
+
+        runBlocking {
+            val visionText = firebaseAnalyzer.processImage(mediaImage).await()
+            visionText.text
+        }
+    }
+}
